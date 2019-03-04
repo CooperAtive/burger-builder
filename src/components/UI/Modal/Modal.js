6cbf655330d4bb5fd649866f53c669f358eb501f
Cooper Trowbridge <cooper.trowbridge@gmail.com>
2019-03-04 08:29:08 -0600

    Modal change to class component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = React.memo((props) => {
-  return (
-    <Aux>
-      <Backdrop show={props.show} clicked={props.modalClosed}/>
-      <div 
-        className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-      </div>
-    </Aux>
-  );
-}, (prevProps, newProps) => newProps.show === prevProps.show);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+  }
 
-export default modal;
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div 
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+};
+
+// how to do the same thing as shouldComponentUpdate with functional component
+// note difference is second argument to memo must return false to trigger update,
+// whereas shouldComponentUpdate (as name suggests) needs to return true
+// const Modal = React.memo((props) => {
+//   return (
+//     <Aux>
+//         <Backdrop show={props.show} clicked={props.modalClosed}/>
+//         <div 
+//           className={classes.Modal}
+//           style={{
+//             transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+//             opacity: props.show ? '1' : '0'
+//           }}>
+//           {props.children}
+//         </div>
+//       </Aux>
+//   )
+// }, (prevProps, nextProps) => {
+//   return prevProps.show === nextProps.show && prevProps.children === nextProps.children
+// });
+
+export default Modal;
