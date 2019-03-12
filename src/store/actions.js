5e285a94f085cfea1d0ed618b87131ab5a32d984
Cooper Trowbridge <cooper.trowbridge@gmail.com>
2019-03-11 22:16:18 -0600

    WIP adding thunk

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,2 +1,17 @@
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
+export const GET_INGREDIENTS_INITIAL = 'GET_INGREDIENTS_INITIAL';
+
+export const getIngredientsInitialActual = () => {
+  return {
+    type: GET_INGREDIENTS_INITIAL
+  }
+}
+
+export const getIngredientsInitial = (payload) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(getIngredientsInitialActual(payload));
+    }, 2000);
+  }
+}
